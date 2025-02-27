9ff0542192dfbde9756118d0d984a85126695454
Junshen18 <junshenhere@gmail.com>
2025-02-27 17:38:27 +0800

    add mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
       {/* Header */}
       <header className="fixed w-full top-0 z-50 bg-transparent backdrop-blur-md ">
         <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-[90px]">
+          <div className="flex items-center justify-between h-[70px] md:h-[90px]">
             {/* Logo */}
-            <Image src="/finova-logo.svg" alt="Logo" width={185} height={42} className="object-contain cursor-pointer"/>
+            <Image src="/finova-logo.svg" alt="Logo" width={185} height={42} className="w-[100px] h-[25px] md:w-[185px] md:h-[42px] object-contain cursor-pointer"/>
 
             {/* Desktop Navigation */}
             <div className="hidden sm:flex sm:items-center sm:space-x-8 font-[inter] font-medium text-base">
@@ -45,19 +45,19 @@ export default function Home() {
         {/* Hero Section */}
         <section className="bg-white py-2 h-[550px]">
           <div className="w-full h-full mx-auto px-4 sm:px-6 lg:px-8 relative flex items-center justify-center">
-            <Image src="/fimi.svg" alt="Fimi" width={300} height={280} className="absolute top-10 left-[50%] -translate-x-1/2 object-cover z-0" />
-            <Image src="/herobg.svg" alt="Hero Background" fill className="absolute top-0 object-cover z-0" />
-            <div className="pt-[230px] text-center z-10 flex flex-col items-center justify-center gap-3">
+            <Image src="/fimi.svg" alt="Fimi" width={300} height={280} className="w-[190px] h-[180px] md:w-[300px] md:h-[280px] absolute top-5 md:top-10 left-[50%] -translate-x-1/2 object-cover z-0" />
+            <Image src="/herobg.svg" alt="Hero Background" fill className="hidden md:block absolute top-0 object-cover z-0" />
+            <div className="pt-[50px] md:pt-[230px] text-center z-10 flex flex-col items-center justify-center gap-3">
               <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl font-[grifterbold]">
                 Your All-in-One <br /> <span className="text-primary">Finance </span>Companion.
               </h1>
-              <p className="max-w-2xl mx-auto text-lg text-black font-medium font-[inter]">
+              <p className="md:max-w-2xl w-[320px] md:w-full mx-auto text-sm md:text-lg text-black font-medium font-[inter]">
               A smarter way to manage your money—track, split, and analyze your expenses effortlessly.
               </p>
 
-                <button className="rounded-full bg-black px-6 py-3 w-[200px] h-[50px] font-medium text-lg font-[inter] text-white">
-                  Get Started
-                </button>
+              <div className="rounded-full bg-black px-6 py-3 w-[150px] h-[40px] md:w-[200px] md:h-[50px] font-medium text-[14px] md:text-lg font-[inter] text-white cursor-pointer flex items-center justify-center">
+                Get Started
+              </div>
             </div>
           </div>
         </section>
